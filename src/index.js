4da4f521e667656d990f712a5af3c710e8ccdc6a
Usman <you@example.com>
2019-08-07 22:31:37 +0200

    Added an input field and a button

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import logo from './add.png';
+  
+  class App extends React.Component {
+    render() {
+      return (
+        <div className="app">
+          <header className="app-header">
+            <h1>todo</h1>
+          </header>
 
-ReactDOM.render(
+          <div className="input">
+            <input type="text" className="inputElement" />
+            <button className="add">
+                <img src={logo} />
+            </button>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  
+  // ========================================
+  
+  ReactDOM.render(
     <App />,
-     document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    document.getElementById('root')
+  );
